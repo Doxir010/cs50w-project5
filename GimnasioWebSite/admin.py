6641caf661da8b885aaa37afca55409b65230c4a
Doxir010 <davidsoza2014@gmail.com>
2024-06-15 17:25:51 -0600

    Creacion de proyecto, config de admin site y de modelos

---
+++ GimnasioWebSite/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Usuario, Plan, Suscripcion, PagoPlan, Asistencia
+from django.contrib.auth.admin import UserAdmin
+
+#
+class usuarioConfig(UserAdmin):
+    list_display = ("id", "first_name", "last_name")
+    search_fields = ("username", "first_name", "last_name")
+    ordering = ("username",)
+
+class planConfig(UserAdmin):
+    list_display = ("id", "nombre", "descripcion", "precio")
+    ordering = ("nombre",)
+    search_fields = ("nombre", "precio")
+
+class suscripcionConfig(UserAdmin):
+    list_display = ("id", "cliente__username", "plan", "fechaInicio", "fechaFinal")
+    ordering = ("fechaInicio",)
+    search_fields = ("cliente__username", "plan__nombre")
+    list_filter = ("plan__nombre", "fechaInicio", "fechaFinal")
+    
+    
+class pagoplanConfig(UserAdmin):
+    list_display = ("id", "cliente", "plan", "fechaPago")
+    
+    
+class asistenciaConfig(UserAdmin):
+    list_display = ("id", "Usuario", "horaEntrada", "horaSalida")
+    ordering = ("horaEntrada",)
+    search_fields = ("Usuario__username", "horaEntrada", "horaSalida")
+
+# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Plan)
+admin.site.register(Suscripcion)
+admin.site.register(PagoPlan)
+admin.site.register(Asistencia)
---
+++ GimnasioWebSite/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Usuario(AbstractUser):
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    telefonoEmergencia = models.CharField(max_length=15, blank=True, null=True)
+    direccion = models.TextField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return f"Nombre: {self.first_name} {self.last_name} // Telefono: {self.telefono} // Dirección: {self.direccion} // Telefono de emergencia: {self.telefonoEmergencia}"
+    
+    
+class Plan(models.Model):
+    nombre = models.CharField(max_length=12)
+    descripcion = models.TextField(max_length=60)
+    duracion = models.IntegerField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre} // Duracion: {self.duracion} // Precio: {self.precio}"
+    
+    
+class Suscripcion(models.Model):
+    cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    fechaInicio = models.DateField()
+    fechaFinal = models.DateField()
+    
+    def __str__(self):
+        return f"Nombre del cliente: {self.cliente} // Plan pagado: {self.plan} // Fecha de inicio del plan: {self.fechaInicio} // Fecha final del plan: {self.fechaFinal}"
+
+
+class PagoPlan(models.Model):
+    cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    fechaPago = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Nombre del cliente: {self.cliente} // Plan pagado: {self.plan} // Fecha de pago: {self.fechaPago}"
+
+class Asistencia(models.Model):
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    horaEntrada = models.DateTimeField(auto_now_add=True)
+    horaSalida = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"Nombre: {self.Usuario.first_name} {self.Usuario.last_name} // Fecha y hora de entrada: {self.horaEntrada} // Fecha y hora de salida: {self.horaSalida}"
